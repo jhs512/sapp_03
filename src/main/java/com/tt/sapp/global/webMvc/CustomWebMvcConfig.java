00f44b301ffbd584617230f962a3ede96b3c1c1e
jhs512 <jangka512@gmail.com>
2024-07-18 00:09:29 +0900

    CORS

---
+++ src/main/java/com/tt/sapp/global/webMvc/CustomWebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.tt.sapp.global.webMvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CustomWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns(
+                        "https://cdpn.io"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
